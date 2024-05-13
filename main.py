5757a262f733d71caf4a1f86fe5ee904ae60db46
Sakan <sakanbeer@gmail.com>
2024-05-13 23:55:08 +0700

    v1.0.0 - Updated web-scraping process - Added specific Exception handlers - Added log with Loguru

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import re
-import logging
 import time
-from loguru import logger
+
 import pandas as pd
 from bs4 import BeautifulSoup
+from loguru import logger
 from selenium import webdriver
 from selenium.common import NoSuchElementException, TimeoutException, StaleElementReferenceException
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
