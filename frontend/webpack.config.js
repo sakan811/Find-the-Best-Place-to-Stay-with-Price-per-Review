97051dd96c3f7e5bcfb983a4771ee4825538c820
Sakan <sakanbeer@gmail.com>
2024-07-14 03:54:31 +0700

    v5.5.5 - Use separate Dockerfile to build backend and frontend - React now fully controls the webpage content - Django now only provides APIs

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ],
   devServer: {
