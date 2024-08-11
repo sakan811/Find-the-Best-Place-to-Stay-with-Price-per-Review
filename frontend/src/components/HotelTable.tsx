a07dda90435b6edaf12606a2576fce9589efb089
Sakan <sakanbeer@gmail.com>
2024-08-11 21:15:13 +0700

    v7.1.0 - Changed source JSX components to TSX - Removed some console.log

---
+++ frontend/src/components/HotelTable.tsx
@@ -0,0 +1,205 @@
+import React, { useState, useEffect } from 'react';
+import { Table, Button, Alert } from 'react-bootstrap';
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
+import './HotelTable.css';
+import { Helmet } from 'react-helmet-async';
+
+interface HotelData {
+    city: string;
+    hotel: string;
+    review_score: number;
+    room_price: number;
+    price_per_review: number;
+    check_in: string;
+    check_out: string;
+}
+
+interface BookingDetail {
+    city: string;
+    check_in: string;
+    check_out: string;
+    num_adults: number;
+    num_children: number;
+    num_rooms: number;
+    currency: string;
+    only_hotel: boolean;
+}
+
+const HotelTable: React.FC = () => {
+    const [data, setData] = useState<HotelData[]>([]);
+    const [bookingDetails, setBookingDetails] = useState<BookingDetail[]>([]);
+    const [successMsg, setSuccessMsg] = useState<boolean>(false);
+    const [errorMsg, setErrorMsg] = useState<boolean>(false);
+
+    useEffect(() => {
+        fetchData();
+        fetchBookingDetails();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get<{ hotel_data: HotelData[] }>('http://localhost:8000/get_hotel_data_from_db/');
+            const responseData: HotelData[] = response.data.hotel_data;
+            setData(responseData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const fetchBookingDetails = async () => {
+        try {
+            const response: AxiosResponse<{ booking_data: BookingDetail[] }> = await axios.get('http://localhost:8000/get_booking_details_from_db/');
+            const responseData: BookingDetail[] = response.data.booking_data;
+            setBookingDetails(responseData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const saveData = async () => {
+        try {
+            const saveDataUrl = 'http://localhost:8000/save/';
+            const data = {};
+            const config: AxiosRequestConfig = { responseType: 'json' };
+
+            const response: AxiosResponse<{ filename: string; file_content: string }> = await axios.post(
+                saveDataUrl,
+                data,
+                config
+            );
+
+            const { filename, file_content } = response.data;
+            const binaryString: string = atob(file_content);
+
+            const bytes = new Uint8Array(binaryString.length);
+            for (let i = 0; i < binaryString.length; i++) {
+                bytes[i] = binaryString.charCodeAt(i);
+            }
+
+            const blob = new Blob([bytes], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+            const url: string = URL.createObjectURL(blob);
+
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', filename);
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+
+            URL.revokeObjectURL(url);
+
+            setSuccessMsg(true);
+            setErrorMsg(false);
+        } catch (error) {
+            console.error('Error saving data:', error);
+            setSuccessMsg(false);
+            setErrorMsg(true);
+        }
+    };
+
+    const goToFormPage = () => {
+        try {
+            window.location.href = '/';
+        } catch (error) {
+            console.error('Error redirecting to form page:', error);
+        }
+    };
+
+    const formatNumber = (value: number | string): string => {
+        const number: number = typeof value === 'string' ? parseFloat(value) : value;
+        return isNaN(number)
+            ? value.toString()
+            : number.toLocaleString(undefined, {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+              });
+    };
+
+    return (
+        <>
+            <Helmet>
+                <title>Find the Best Place to Stay with Price/Review - Table</title>
+            </Helmet>
+            <Button variant="primary" className="backToFormButton" onClick={goToFormPage}>
+                Back to Form
+            </Button>
+            <h2 style={{ textAlign: 'center' }}>Hotels' Room Price/Review Data</h2>
+            <p style={{ textAlign: 'center', fontSize: '16px' }}>
+                A lower Price/Review Score indicates that the place isn't expensive, yet, with a great review score
+            </p>
+            <Button variant="success" className="saveDataButton" onClick={saveData}>
+                Save Data to Excel
+            </Button>
+
+            {successMsg && (
+                <Alert variant="success" className="message">
+                    <strong>Success!</strong> Saved data to Excel successfully.
+                </Alert>
+            )}
+            {errorMsg && (
+                <Alert variant="danger" className="errorMessage">
+                    <strong>Error!</strong> Failed to save data.
+                </Alert>
+            )}
+
+            <h3 style={{ marginBottom: '5px' }}>Booking Details</h3>
+            {bookingDetails.length > 0 && (
+                <div className="booking-details">
+                    <p className="detail-item">
+                        <strong>City</strong>: {bookingDetails[0].city}
+                    </p>
+                    <p className="detail-item">
+                        <strong>Check In</strong>: {bookingDetails[0].check_in}
+                    </p>
+                    <p className="detail-item">
+                        <strong>Check Out</strong>: {bookingDetails[0].check_out}
+                    </p>
+                    <p className="detail-item">
+                        <strong>Adults</strong>: {bookingDetails[0].num_adults}
+                    </p>
+                    <p className="detail-item">
+                        <strong>Children</strong>: {bookingDetails[0].num_children}
+                    </p>
+                    <p className="detail-item">
+                        <strong>Rooms</strong>: {bookingDetails[0].num_rooms}
+                    </p>
+                    <p className="detail-item">
+                        <strong>Currency</strong>: {bookingDetails[0].currency}
+                    </p>
+                    <p className="detail-item">
+                        <strong>Only Hotel Properties</strong>: {bookingDetails[0].only_hotel.toString()}
+                    </p>
+                </div>
+            )}
+
+            <Table striped bordered hover className="hotelTable">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Hotel</th>
+                        <th>Review</th>
+                        <th>Price</th>
+                        <th>Price/Review</th>
+                        <th>Check In</th>
+                        <th>Check Out</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((hotel, index) => (
+                        <tr key={index}>
+                            <td>{hotel.city}</td>
+                            <td>{hotel.hotel}</td>
+                            <td>{formatNumber(hotel.review_score)}</td>
+                            <td>{formatNumber(hotel.room_price)}</td>
+                            <td>{formatNumber(hotel.price_per_review)}</td>
+                            <td>{hotel.check_in}</td>
+                            <td>{hotel.check_out}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </>
+    );
+};
+
+export default HotelTable;
---
+++ frontend/src/components/ScrapingForm.tsx
@@ -0,0 +1,191 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import './ScrapingForm.css';
+import axios from 'axios';
+import { Helmet } from 'react-helmet-async';
+
+// Define the shape of form data
+interface FormData {
+  city: string;
+  check_in: string;
+  check_out: string;
+  group_adults: number;
+  num_rooms: number;
+  group_children: number;
+  selected_currency: string;
+  hotel_filter: boolean;
+}
+
+const ScrapingForm: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    city: '',
+    check_in: '',
+    check_out: '',
+    group_adults: 1,
+    num_rooms: 1,
+    group_children: 0,
+    selected_currency: '',
+    hotel_filter: false,
+  });
+
+  const [submitMessage, setSubmitMessage] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [scrapingMessage, setScrapingMessage] = useState<string>('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setScrapingMessage('Scraping data...');
+    setErrorMessage(''); // Clear any previous error messages
+
+    try {
+      await axios.post('http://localhost:8000/scraping/', formData);
+      setSubmitMessage('Form submitted successfully');
+      window.location.href = '/hotel_data_table_page/';
+    } catch (error: any) { // use 'any' type to allow flexible error handling
+      if (error.response) {
+        console.error('Error response:', error.response.data);
+        console.error('Status code:', error.response.status);
+
+        if (error.response.data.error === 'SystemExit') {
+          setErrorMessage('No places found that can satisfy this booking. Please re-enter the form.');
+        } else if (error.response.status === 500) {
+          setErrorMessage('Internal server error. Please try again later.');
+        } else {
+          setErrorMessage('An error occurred while submitting the form.');
+        }
+      } else if (error.request) {
+        console.error('No response received:', error.request);
+        setErrorMessage('No response received from the server. Please check your internet connection.');
+      } else {
+        console.error('Error setting up the request:', error.message);
+        setErrorMessage('An unexpected error occurred. Please try again later.');
+      }
+    } finally {
+      setScrapingMessage('');
+    }
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Find the Best Place to Stay with Price/Review - Form</title>
+      </Helmet>
+      <h1 style={{ textAlign: 'center' }}>Finding the Best Place to Stay with Price/Review from Booking.com</h1>
+      <p style={{ textAlign: 'center', fontSize: '16px' }}>
+        Enter the hotel booking details below to scrape the hotel data
+      </p>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="formGroup">
+          <label htmlFor="city">City:</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+            maxLength={100}
+            className="input"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="check_in">Check-in:</label>
+          <input
+            type="date"
+            id="check_in"
+            name="check_in"
+            value={formData.check_in}
+            onChange={handleChange}
+            className="input"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="check_out">Check-out:</label>
+          <input
+            type="date"
+            id="check_out"
+            name="check_out"
+            value={formData.check_out}
+            onChange={handleChange}
+            className="input"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="group_adults">Adults:</label>
+          <input
+            type="number"
+            id="group_adults"
+            name="group_adults"
+            value={formData.group_adults}
+            onChange={handleChange}
+            min={1}
+            className="input"
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="num_rooms">Rooms:</label>
+          <input
+            type="number"
+            id="num_rooms"
+            name="num_rooms"
+            value={formData.num_rooms}
+            onChange={handleChange}
+            min={1}
+            className="input"
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="group_children">Children:</label>
+          <input
+            type="number"
+            id="group_children"
+            name="group_children"
+            value={formData.group_children}
+            onChange={handleChange}
+            min={0}
+            className="input"
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="selected_currency">Currency:</label>
+          <input
+            type="text"
+            id="selected_currency"
+            name="selected_currency"
+            value={formData.selected_currency}
+            onChange={handleChange}
+            maxLength={10}
+            className="input"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="hotel_filter">Scrape Hotel Properties Only:</label>
+          <input
+            type="checkbox"
+            id="hotel_filter"
+            name="hotel_filter"
+            checked={formData.hotel_filter}
+            onChange={handleChange}
+            className="checkbox"
+          />
+        </div>
+        <button type="submit" className="submitButton">Submit</button>
+        {scrapingMessage && <p className="message">{scrapingMessage}</p>}
+        {submitMessage && <p className="message">{submitMessage}</p>}
+        {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+      </form>
+    </>
+  );
+};
+
+export default ScrapingForm;
