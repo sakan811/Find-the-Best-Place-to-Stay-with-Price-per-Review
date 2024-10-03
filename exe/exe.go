cccbd50eea0d35a60829a142d2fdacce5338758c
Sakan Nirattisaykul <94357278+sakan811@users.noreply.github.com>
2024-10-03 16:51:22 +0700

    Update exe.go

--- exe/exe.go
+++ exe/exe.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 // checkDockerRunning checks if Docker is running
@@ -26,21 +27,35 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Get the directory of the executable
+	exePath, err := os.Executable()
+	if err != nil {
+		fmt.Printf("Error getting executable path: %v\n", err)
+		waitForUser()
+		os.Exit(1)
+	}
+	exeDir := filepath.Dir(exePath)
+
+	// Change to the directory of the executable
+	err = os.Chdir(exeDir)
+	if err != nil {
+		fmt.Printf("Error changing to executable directory: %v\n", err)
+		waitForUser()
+		os.Exit(1)
+	}
+
 	// Run docker-compose in detached mode (-d flag)
 	cmd := exec.Command("docker", "compose", "up", "-d")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Dir = "./"
-
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		waitForUser() // Keeps the terminal open
+		fmt.Printf("Error running Docker Compose: %v\n", err)
+		waitForUser()
 		os.Exit(1)
 	}
 
 	fmt.Println("Please navigate to \033[34mhttp://localhost:5000/\033[0m to access the web-app.")
-
 	fmt.Println("Docker containers are running in detached mode.")
 	waitForUser() // Wait for the user before closing the terminal
 }
