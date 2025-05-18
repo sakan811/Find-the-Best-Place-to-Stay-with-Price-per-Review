990932c2518369edbb2010246d95ffa45150edeb
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-05-18 17:52:46 +0700

    feat: add link to compare page in AddHotelPage component

--- src/app/hotels/add/page.tsx
+++ src/app/hotels/add/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function AddHotelPage() {
   const router = useRouter();
@@ -214,6 +215,14 @@ export default function AddHotelPage() {
           >
             Submit & Compare
           </button>
+        </div>        
+          <div className="flex justify-center mt-4.5">
+          <Link 
+            href="/hotels/compare"
+            className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 text-center" 
+          >
+            Compare Page
+          </Link>
         </div>
       </form>
     </div>
